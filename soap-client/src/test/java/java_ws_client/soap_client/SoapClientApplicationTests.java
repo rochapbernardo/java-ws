160e0f6a3bc3219582bfd209e458336340e92f26
rochapbernardo <bernardopolese2809@gmail.com>
2025-11-01 17:30:23 -0300

    initial commit client layer

---
+++ soap-client/src/test/java/java_ws_client/soap_client/SoapClientApplicationTests.java
@@ -0,0 +1,13 @@
+package java_ws_client.soap_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
