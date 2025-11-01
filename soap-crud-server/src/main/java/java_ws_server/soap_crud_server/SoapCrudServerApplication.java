67519255f49f8ab36369613f710b7e86cea572a8
rochapbernardo <bernardopolese2809@gmail.com>
2025-11-01 15:40:18 -0300

    initial commit with the springboot project files

---
+++ soap-crud-server/src/main/java/java_ws_server/soap_crud_server/SoapCrudServerApplication.java
@@ -0,0 +1,13 @@
+package java_ws_server.soap_crud_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapCrudServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapCrudServerApplication.class, args);
+	}
+
+}
