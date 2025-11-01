79f8ab032c9f79d2ce7304da741d2e8248829812
rochapbernardo <bernardopolese2809@gmail.com>
2025-11-01 16:36:09 -0300

    create config JaxWsConfig.java

---
+++ soap-crud-server/src/main/java/java_ws_server/soap_crud_server/config/JaxWsConfig.java
@@ -0,0 +1,20 @@
+package java_ws_server.soap_crud_server.config;
+
+import jakarta.xml.ws.Endpoint;
+import java_ws_server.soap_crud_server.endpoint.UserEndpoint;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JaxWsConfig {
+    private final UserEndpoint userEndpoint;
+
+    public JaxWsConfig(UserEndpoint endpoint) {
+        this.userEndpoint = endpoint;
+    }
+
+    @Bean
+    public Endpoint userServiceEndpoint(){
+        return Endpoint.publish("http://localhost:8080/ws/users", userEndpoint);
+    }
+}
