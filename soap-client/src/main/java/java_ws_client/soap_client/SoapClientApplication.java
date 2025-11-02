e637693200da5fa1a4edf8800d028d3527935aef
rochapbernardo <bernardopolese2809@gmail.com>
2025-11-02 16:04:33 -0300

    updated app to be executed

--- soap-client/src/main/java/java_ws_client/soap_client/SoapClientApplication.java
+++ soap-client/src/main/java/java_ws_client/soap_client/SoapClientApplication.java
@@ -6,8 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SoapClientApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SoapClientApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SoapClientApplication.class, args);
+        UserClient client = new UserClient();
+        client.run();
+    }
 
 }
